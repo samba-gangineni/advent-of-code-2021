77631a137769f82a50ef635b3235efcd8e76e4ec
samba-gangineni <sambasivarao693@gmail.com>
2021-12-01 22:22:36 -0500

    Day1Prob2

--- Solve/Program.cs
+++ Solve/Program.cs
@@ -10,7 +10,9 @@ namespace Solve
         {
             string filePath = Path.Combine(Environment.CurrentDirectory, "data", "Day1Prob1.txt");
             Day1Prob1 prob1 = new Day1Prob1();
+            Day1Prob2 prob2 = new Day1Prob2();
             prob1.Solution(filePath);
+            prob2.Solution(filePath);
         }
     }
 }
