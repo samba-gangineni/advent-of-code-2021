a00f565b1ca0cf9c8e2a756ee258a07524d9a4fb
samba-gangineni <sambasivarao693@gmail.com>
2021-12-02 09:54:20 -0500

    Ability to publish single package for mac and updating the instructions

--- README.md
+++ README.md
@@ -1,23 +1,44 @@
 # AdventOfCode 2021
 
-### Requirements:
+## Requirements:
 
-- .NET 5.0
-- Git
+- [.NET 5.0](https://dotnet.microsoft.com/download/dotnet/5.0)
+- [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
 
-### Usage:
+## Usage:
 
-1. Clone the repo
+### Clone the repo
 
 ```bash
 $ git clone https://github.com/samba-gangineni/advent-of-code-2021.git
 ```
 
-2. run the program
+### run the project
 
 ```bash
 $ cd advent-of-code-2021
 $ dotnet run --project Solve/Solve.csproj [Day] [Problem]
 # [Day]: 1 - 25
 # [Prob]: 1 - 2
+```
+
+### publish the project
+
+```bash
+$ dotnet publish
+# Change the configurations in Solve.csproj to get executables for different run times and os
+```
+
+### run the package
+
+- Download the package `Solve` from aforementioned github repo.
+
+Note: Naming convention for data files is `Day1Prob.txt`. You can download the data from the aforementioned repo
+
+```bash
+# Set environment variable to path where your data files are present
+$ export AOC_2021_DATA="/Users/sambagangineni/Documents/samba-gangineni/AdventOfCode2021/data"
+$ cd [Folder_where_package_is_downloaded]
+# Ex: cd ~/Downloads
+$ ./Solve [Day] [Problem]
 ```
--- Solve/Program.cs
+++ Solve/Program.cs
@@ -13,7 +13,9 @@ namespace Solve
                 // Argument validation
                 if (args.Length != 2)
                 {
-                    Console.WriteLine("Usage: dotnet run --project Solve/Solve.csproj [Day] [Prob]");
+                    Console.WriteLine("Usage: dotnet run --project Solve/Solve.csproj [Day] [Prob]\n");
+                    Console.WriteLine("If you are using published package:");
+                    Console.WriteLine("Usage: ./Solve [Day] [Prob]");
                     Environment.Exit(1);
                 }
                 int day = int.Parse(args[0]);
@@ -23,7 +25,7 @@ namespace Solve
                     throw new ArgumentException("Please check command line arguments");
                 }
 
-                string filePath = Path.Combine(Environment.CurrentDirectory, "data", $"Day{day}Prob.txt");
+                string filePath = Path.Combine(Environment.GetEnvironmentVariable("AOC_2021_DATA"), $"Day{day}Prob.txt");
                 // Write the logic to download the file directly
                 Type problemType = Type.GetType($"ChallangeLib.Day{day}Prob{prob},ChallangeLib");
                 DayProb problem = (Activator.CreateInstance(problemType)) as DayProb;
